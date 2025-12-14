2d6de1e55f370af9016a74ebc9c27d011b8b1568
Unknowns101 <rbntambunan@gmail.com>
2025-12-14 22:41:32 +0700

    Update main.cpp
    final banget main nya dah beres akhirnya

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
-//test dulu
+#include <iostream>
+#include"TubesSpotify.h"
+using namespace std;
+
+int main() {
+    Roles R;
+    Users U;
+    Playlists P;
+    Songs S;
+    string admin;
+    string user;
+    addressRole adminRole;
+    addressRole userRole;
+
+    createRoles(R);
+    createUsers(U);
+    createPlaylists(P);
+    createSongs(S);
+
+    cout<<"admin";
+    cin>>admin;
+    cout<<endl;
+    cout<<"user";
+    cin>>user;
+    cout<<endl;
+    adminRole = allocateRole(admin);
+    userRole  = allocateRole(user);
+
+    insertRole(R, adminRole);
+    insertRole(R, userRole);
+
+    menuAwal(R, U, P, S);
+
+    return 0;
+}
